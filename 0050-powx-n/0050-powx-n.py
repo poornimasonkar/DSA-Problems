ecfebbd4c17e68e0699abf5cc834d4dda0ba6bdc
Poornima Sonkar <sonkarashok72ashok@gmail.com>
2026-08-24 11:01:13 +0530

    Time: 1 ms (9.79%), Space: 12.5 MB (28.31%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+           
+            return 1
+        
+        return x * x**(n-1)
+
+        
